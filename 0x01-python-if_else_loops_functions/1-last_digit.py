a9dc76abd73b7b540bf79a026b307b348ce27451
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2024-01-03 13:47:01 +0100

    Get the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = int(str(number)[-1])
+if number < 0:
+    last_digit *= -1
+
+if last_digit > 5:
+    remark = "and is greater than 5"
+elif last_digit == 0:
+    remark = "and is 0"
+else:
+    remark = "and is less than 6 and not 0"
+print(f"Last digit of {number} is {last_digit} {remark}")
