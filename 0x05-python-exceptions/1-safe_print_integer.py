567e8970aaaa8e7ca83ebf7f619738a781476b4e
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2024-02-12 14:33:36 +0100

    Print only integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """
+        Print only integer
+    """
+
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return 0
+    else:
+        return 1
