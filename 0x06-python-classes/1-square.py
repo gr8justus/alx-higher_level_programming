07a712dd720d02e5dad05a3238f1c5e05946ac85
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2024-02-22 14:46:51 +0100

    Instantiate class square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    This module contains class square
+"""
+
+
+class Square:
+    """
+        This class defines a square
+
+        Args:
+            size: the size of square
+
+        Attributes:
+            size: the size of square
+    """
+
+    def __init__(self, size):
+        self.__size = size
