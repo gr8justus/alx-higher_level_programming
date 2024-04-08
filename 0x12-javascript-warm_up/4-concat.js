fd041c7c4d9bcc99e7fca5dd5fbc10e9a4be2626
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2024-04-09 00:04:54 +0100

    Concat two arguments with "is"

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const args = process.argv;
+const firstArgIndex = 2;
+const secondArgIndex = 3;
+const printArgs = `${args[firstArgIndex]} is ${args[secondArgIndex]}`;
+
+console.log(printArgs);
