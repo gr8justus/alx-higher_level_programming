2537dd4e4e0245e4483dbbcdaec99a2162e572af
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2024-04-09 00:54:33 +0100

    Loop and print string to console

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const strArray = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let iterator = 0; iterator < strArray.length; iterator++) {
+  console.log(strArray[iterator]);
+}
